262cf224d4afddf3551e66415d4fdd066975031f
Diego Subires <diegosubires@gmail.com>
2024-10-17 18:59:01 +0200

    subir a repositorio

--- src/personajes-listado.ts
+++ src/personajes-listado.ts
@@ -1,8 +1,8 @@
 import { CrearBotonParams, Personaje } from "./personajes-listado.model";
 import { obtenerPersonajes } from "./personajes-listado.api";
 
-export let imputForm: string = "morta";
-console.log(imputForm);
+export let inputForm: string = "";
+export const inputTextElement = document.getElementById("nombre");
 
 export const crearElementoImagen = (
   portada: string,
@@ -31,8 +31,6 @@ export const crearElementoParrafo = (
   return div;
 };
 
-//// NO REVISADO
-
 export const crearBoton = (
   crearBotonParams: CrearBotonParams
 ): HTMLButtonElement => {
@@ -45,27 +43,6 @@ export const crearBoton = (
   boton.classList.add(nombreClase);
   return boton;
 };
-
-/*export const crearGrupoBotones = (id: string): HTMLDivElement => {
-  const grupoBotones = document.createElement("div");
-  grupoBotones.classList.add("grupo-botones");
-  const botonEditar = crearBoton({
-    texto: "Editar",
-    id: id,
-    nombreClase: "boton-editar",
-    onClick: () => editaPelicula(id),
-  });
-  const botonBorrar = crearBoton({
-    texto: "Borrar",
-    id: id,
-    nombreClase: "boton-borrar",
-    onClick: () => borraPelicula(id),
-  });
-  grupoBotones.appendChild(botonEditar);
-  grupoBotones.appendChild(botonBorrar);
-  return grupoBotones;
-};*/
-//// Revisado
 
 export const crearContenedorPersonaje = (
   personaje: Personaje
@@ -111,7 +88,9 @@ export const pintarPersonajes = async (str: string): Promise<void> => {
 document.addEventListener("DOMContentLoaded", () => {
   handleClickFiltrar;
 });
+
 pintarPersonajes("");
+
 export const obtenerValorCampo = (campo: string): string => {
   const elementoCampo = document.querySelector(`#${campo}`);
   if (elementoCampo && elementoCampo instanceof HTMLInputElement) {
@@ -123,7 +102,7 @@ export const obtenerValorCampo = (campo: string): string => {
 
 export const handleClickFiltrar = async (evento: Event) => {
   evento.preventDefault();
-  imputForm = obtenerValorCampo("nombre");
+  inputForm = obtenerValorCampo("nombre");
 
   const div = document.getElementById("listado-personajes");
   if (div && div instanceof HTMLDivElement) {
@@ -131,47 +110,12 @@ export const handleClickFiltrar = async (evento: Event) => {
       div.removeChild(div.firstChild);
     }
   }
-
-  pintarPersonajes(imputForm);
+  if (inputTextElement && inputTextElement instanceof HTMLInputElement) {
+    inputTextElement.value = "";
+  }
+  pintarPersonajes(inputForm);
 };
 
 const botonFiltrar = document.getElementById("botonFiltrar");
 if (botonFiltrar && botonFiltrar instanceof HTMLButtonElement)
   botonFiltrar.addEventListener("click", handleClickFiltrar);
-
-/* export const editaPelicula = async (id: string) => {
-  window.location.href = `../pelicula-editar/index.html?
-  id=${encodeURIComponent(id)}`;
-};
-export const borraPelicula = async (id: string) => {
-  try {
-    await borrarPelicula(id);
-    const listado = document.querySelector("#listado-peliculas");
-    if (listado && listado instanceof HTMLDivElement) {
-      listado.innerHTML = "";
-      pintarPeliculas();
-    } else {
-      throw new Error("No se ha encontrado el contenedor del listado");
-    }
-    alert("Pelicula borrada con exito");
-  } catch (error) {
-    alert(error);
-  }
-};*/
-/*
-const actualizaImput = async (evento: Event): Promise<string> => {
-  evento.preventDefault();
-  imputForm = obtenerValorCampo("nombre");
-  console.log(imputForm);
-  return imputForm;
-};
-
-const iniciarFormulario = () => {
-  const formulario = document.querySelector("#formulario");
-
-  if (formulario && formulario instanceof HTMLFormElement) {
-    formulario.addEventListener("submit", actualizaImput);
-  } else {
-    throw new Error("No se ha encontrado el formulario");
-  }
-};*/
